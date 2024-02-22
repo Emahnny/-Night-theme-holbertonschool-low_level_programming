e7699457cbb2e161ee5af32d2235c6da2b49e13b
Emmanuel <3ma195@gmail.com>
2024-02-21 23:25:32 -0400

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - function that prints 0
  *
- * Return: Alway 0 (Sucess)
+ * Return: 0;
  */
 
 int main(void)
 {
-	char *h = _putchar";
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar(\n);
 
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
-	return (0);
+		return (0);
+
 }
-
-
