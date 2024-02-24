60761750c85c00c7d83d96ff290d22cd1f0fa196
Emmanuel <3ma195@gmail.com>
2024-02-24 18:18:25 -0400

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,20 @@
 /**
 *print_most_numbers - prints numbers except 2 and 4
 *
-*Return: returns nothing
+*Return: void
 */
 
 void print_most_numbers(void)
 
-int e;
-for (e = 48; number < 58; e++)
 {
-if ((e == 50) || (e == 52))
+	int e;
+for (e = 48 ; e < 58 ; e++)
+{
+if (e == 50 && e == 52)
 {
 continue;
 }
-_putchar(e);
+_putcha(e);
 }
 _putchar(10);
-}
+
