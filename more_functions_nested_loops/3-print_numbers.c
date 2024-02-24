38c59ba3589a2f47884eda85536feccd902bef9a
Emmanuel <3ma195@gmail.com>
2024-02-24 17:59:47 -0400

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int e;
+for (e = 0 ; e < 9 ; e++)
+	_putchar(e + 0);
+		_putchar(\n);
+}
