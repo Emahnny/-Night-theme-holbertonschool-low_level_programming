d3edda46632f07dabad9e112c65067bfacb3f6d0
Emmanuel <3ma195@gmail.com>
2024-02-26 19:13:53 -0400

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-/**/
+/*
+ *main - change value of 'n to 98
+ *
+ *Return: 0
+ *
+ **/
 
 int main(void)
 {
 int n;
 n = 402;
-
 int *miparametro = &n;
 *miparametro = 98;
-
-return 0;
+return (0);
 }
 
