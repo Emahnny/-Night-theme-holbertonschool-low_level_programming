ebd5d1e94337bc68a78c6687c9691a54595a5eaa
Emmanuel <3ma195@gmail.com>
2024-02-27 18:32:15 -0400

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include 'main.c'
+
+/**
+ * char *_strcpy - a function that copy the string poited to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int e = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		1++;
+	}
+		for ( ; e < 1 ; e++)
+		{
+			dest[e] = src{e};
+		}
+		dest[1] = '\0'
+		return (dest);
+}
+
+
