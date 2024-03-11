94733395db0de71c7705afddbe57331412a6ff2b
Emmanuel <3ma195@gmail.com>
2024-03-11 13:53:09 -0400

    dog h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,15 +2,17 @@
 #define DOG
 #include <stdio.h>
 #include <stdlib.h>
-
-
+/**
+ * struct dog - Give info about dog
+ * @name: dog name
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
-/* define a new type stuck dog with the following element*/
 };
 
 #endif
-
