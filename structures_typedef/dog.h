f0bc7e1108a098f95fd1b3f4c53b5f46b5d41df1
Emmanuel <3ma195@gmail.com>
2024-03-11 13:23:16 -0400

    starting C-structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+/* define a new type stuck dog with the following element*/
+};
+
+#endif
+
