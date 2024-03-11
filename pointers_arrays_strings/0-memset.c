0c6b4357505ffa31ece97aa4401402333761168e
Emmanuel <3ma195@gmail.com>
2024-03-10 23:14:38 -0400

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte
- * @s: first value
- * @b: second value
- * @n: third value
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * Return: char with result of memset
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-	if (s == NULL)
-{
-	return (NULL);
-}
-
-{
-	for (unsigned int i = 0; i < n; i++)
-
-	:	s[i] = b;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
 	}
+
 	return (s);
 }
